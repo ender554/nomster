646aee8ec37cf09797ccc8f41ba182a84b9972a3
ender554 <joshadams554@gmail.com>
2017-04-25 18:57:11 -0700

    lesson 12 added pagination

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,10 @@
 class Place < ApplicationRecord
+
+  def self.search(place, page)
+  if place
+    where('name LIKE ?', "%#{place}%").paginate(page: page, per_page: 5).order('id')
+  else
+    paginate(page: page, per_page: 5).order('id') 
+  end
+end
 end
