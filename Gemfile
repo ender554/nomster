09d752b364af6655a0991853f96332be8f19de55
ender554 <joshadams554@gmail.com>
2017-04-25 19:07:48 -0700

    lesson 13 created form for adding place

--- Gemfile
+++ Gemfile
@@ -56,3 +56,4 @@ source 'https://rails-assets.org' do
   gem 'rails-assets-tether', '>= 1.3.3'
 end
 gem 'will_paginate'
+gem 'simple_form'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -2,4 +2,8 @@ class PlacesController < ApplicationController
   def index
     @places = Place.search(params[:term], params[:page])
   end
+
+  def new
+    @place = Place.new
+  end
 end
